19d713a07cbb32f8f151df924b2768232aeb6b70
muthuraj-mariappan <70747268+muthuraj-mariappan@users.noreply.github.com>
2024-03-28 15:23:54 +0530

    TK-89152 - list of releases frontend changes (#2)
    * initial frontend changes
    
    * release home plugin home page
    
    * fix tsc error
    
    * unit tests
    
    * fix the name
    
    * fixing warning message while running test
    
    * fixing test
    
    * Updating the yarn test failure issue
    
    ---------
    
    Co-authored-by: Ishwarya Surendrababu <sishwarya@digital.ai>

---
+++ plugins/dai-release/src/components/DenseTable/DenseTable.test.tsx
@@ -0,0 +1,94 @@
+import { TestApiProvider, renderInTestApp } from '@backstage/test-utils';
+import { DenseTable } from '../DenseTable';
+import React from 'react';
+import { TableColumn } from '@backstage/core-components';
+
+describe('DenseTable', () => {
+  const columns: TableColumn[] = [
+    {
+      title: 'Column-A',
+      field: 'firstName',
+    },
+    {
+      title: 'Column-B',
+      field: 'lastName',
+    },
+  ];
+
+  it('should render for empty content', async () => {
+    const rendered = await renderContent({
+      columns: columns,
+      loading: false,
+      page: 1,
+      pageSize: 5,
+      tableData: [],
+      totalCount: 0,
+      onPageChange: () => {},
+      onRowsPerPageChange: () => {},
+      retry: () => {},
+    });
+    columns.forEach(c =>
+      expect(rendered.getByText(c.title as string)).toBeInTheDocument(),
+    );
+    expect(rendered.getByText('No releases available')).toBeInTheDocument();
+    expect(rendered.getByText('5 rows')).toBeInTheDocument();
+    expect(rendered.queryAllByText('0-0 of 0').length > 0).toBeTruthy();
+  });
+
+  it('should render for valid content', async () => {
+    const data = [
+      {
+        firstName: 'John',
+        lastName: 'Doe',
+      },
+    ];
+    const rendered = await renderContent({
+      columns: columns,
+      loading: false,
+      page: 1,
+      pageSize: 5,
+      tableData: data,
+      totalCount: data.length,
+      onPageChange: () => {},
+      onRowsPerPageChange: () => {},
+      retry: () => {},
+    });
+    columns.forEach(c =>
+      expect(rendered.getByText(c.title as string)).toBeInTheDocument(),
+    );
+    data.forEach(d => {
+      expect(rendered.getByText(d.firstName)).toBeInTheDocument();
+      expect(rendered.getByText(d.lastName)).toBeInTheDocument();
+    });
+    expect(rendered.getByText('5 rows')).toBeInTheDocument();
+    expect(rendered.queryAllByText('1-1 of 1').length > 0).toBeTruthy();
+  });
+});
+
+async function renderContent(args: {
+  tableData: any[];
+  loading: boolean;
+  page: number;
+  pageSize: number;
+  totalCount: number;
+  onPageChange: (page: number) => void;
+  onRowsPerPageChange: (rows: number) => void;
+  retry: () => void;
+  columns: TableColumn[];
+}) {
+  return await renderInTestApp(
+    <TestApiProvider apis={[]}>
+      <DenseTable
+        page={args.page}
+        columns={args.columns}
+        loading={args.loading}
+        pageSize={args.pageSize}
+        tableData={args.tableData}
+        totalCount={args.totalCount}
+        onPageChange={args.onPageChange}
+        onRowsPerPageChange={args.onRowsPerPageChange}
+        retry={args.retry}
+      />
+    </TestApiProvider>,
+  );
+}
--- plugins/dai-release/src/plugin.ts
+++ plugins/dai-release/src/plugin.ts
@@ -1,22 +1,28 @@
+import { DaiReleaseApiClient, daiReleaseApiRef } from './api';
 import {
+  createApiFactory,
   createPlugin,
   createRoutableExtension,
+  discoveryApiRef,
 } from '@backstage/core-plugin-api';
-
-import { rootRouteRef } from './routes';
+import { daiReleaseContentRouteRef } from './routes';
 
 export const daiReleasePlugin = createPlugin({
   id: 'dai-release',
-  routes: {
-    root: rootRouteRef,
-  },
+  apis: [
+    createApiFactory({
+      api: daiReleaseApiRef,
+      deps: { discoveryApi: discoveryApiRef },
+      factory: ({ discoveryApi }) => new DaiReleaseApiClient({ discoveryApi }),
+    }),
+  ],
 });
 
 export const DaiReleasePage = daiReleasePlugin.provide(
   createRoutableExtension({
     name: 'DaiReleasePage',
     component: () =>
-      import('./components/ExampleComponent').then(m => m.ExampleComponent),
-    mountPoint: rootRouteRef,
+      import('./components/HomePageComponent').then(m => m.HomePageComponent),
+    mountPoint: daiReleaseContentRouteRef,
   }),
 );
