6faeeabb2b1439cafd94b4da0cfa669a55ff3b5b
Karthigaiselvan <karthikslvn@gmail.com>
2024-03-30 18:07:52 +0530

    filter design update

--- plugins/dai-release/src/components/SearchFilter/SearchFilter.tsx
+++ plugins/dai-release/src/components/SearchFilter/SearchFilter.tsx
@@ -37,20 +37,23 @@ export const SearchFilter = () => {
       margin: theme.spacing('auto', 1, 'auto', 0),
       width: theme.spacing(1.5),
     },
-    filterFont: {
-      height: '120px',
-      width: '100px',
+    inputRoot: {
+      fontSize: '12px',
+    },
+    datePickerInputRoot: {
+      fontSize: '12px',
+      width: '180px',
+    },
+    inputLabelRoot: {
+      fontSize: '12px',
+    },
+    openCalenderPickerIcon: {
+      height: '16px',
+      width: '16px',
     },
   }));
   const classes = useStyles();
 
-  const MenuProps = {
-    PaperProps: {
-      style: {
-        width: 250,
-      },
-    },
-  };
   const onStatusChange = (event: SelectChangeEvent<typeof statusList>) => {
     const {
       target: { value },
@@ -62,89 +65,153 @@ export const SearchFilter = () => {
   };
   return (
     <Paper elevation={1}>
-      <Grid
-        container
-        spacing={1}
-        direction="row"
-        justifyContent="flex-start"
-        alignItems="center"
-        wrap="nowrap"
-        lg={12}
-      >
-        <Grid item lg={2}>
-          <TextField
-            id="outlined-basic"
-            label="Title"
-            variant="outlined"
-            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-              console.log(event);
-            }}
-            size={'small'}
-          />
-        </Grid>
-        <Grid item lg={3}>
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DateTimePicker
-              slotProps={{ textField: { size: 'small' } }}
-              label="Start"
-              ampm={true}
-              value={start}
+      <FormControl sx={{ mx: 1, my: 1.5 }}>
+        <Grid
+          container
+          spacing={1}
+          direction="row"
+          justifyContent="flex-start"
+          alignItems="center"
+        >
+          <Grid item lg={2}>
+            <TextField
+              id="outlined-basic"
+              label="Title"
+              variant="outlined"
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                console.log(event);
+              }}
+              size={'small'}
+              InputProps={{
+                classes: {
+                  root: classes.inputRoot,
+                },
+              }}
+              InputLabelProps={{ classes: { root: classes.inputLabelRoot } }}
             />
-          </LocalizationProvider>
-        </Grid>
-        <Grid item lg={3}>
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DateTimePicker
-              slotProps={{ textField: { size: 'small' } }}
-              label="To"
-              ampm={true}
-              value={end}
-            />
-          </LocalizationProvider>
-        </Grid>
-        <Grid item lg={3}>
-          <FormControl sx={{ my: 1, width: 250 }} size="small">
-            <InputLabel id="status-multiple-checkbox-label">Tag</InputLabel>
-            <Select
-              labelId="status-multiple-checkbox-label"
-              id="status-multiple-checkbox"
-              multiple
-              value={statusList}
-              onChange={onStatusChange}
-              input={<OutlinedInput label="Tag" />}
-              renderValue={selected => selected.join(', ')}
-              MenuProps={MenuProps}
-              inputProps={{ size: 'small' }}
-            >
-              {statuses.map(data => (
-                <MenuItem key={data.status} value={data.status}>
-                  <Checkbox checked={statusList.indexOf(data.status) > -1} />
-                  <i
-                    className={classes.statusIcon}
-                    style={{ backgroundColor: data.color }}
+          </Grid>
+          <Grid item>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DateTimePicker
+                slotProps={{
+                  textField: {
+                    size: 'small',
+                    InputProps: {
+                      classes: {
+                        root: classes.datePickerInputRoot,
+                      },
+                    },
+                    InputLabelProps: {
+                      classes: { root: classes.inputLabelRoot },
+                    },
+                  },
+                  openPickerIcon: {
+                    classes: { root: classes.openCalenderPickerIcon },
+                  },
+                }}
+                label="Start"
+                ampm={true}
+                value={start}
+              />
+            </LocalizationProvider>
+          </Grid>
+          <Grid item>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DateTimePicker
+                slotProps={{
+                  textField: {
+                    size: 'small',
+                    InputProps: {
+                      classes: {
+                        root: classes.datePickerInputRoot,
+                      },
+                    },
+                    InputLabelProps: {
+                      classes: { root: classes.inputLabelRoot },
+                    },
+                  },
+                  openPickerIcon: {
+                    classes: { root: classes.openCalenderPickerIcon },
+                  },
+                }}
+                label="To"
+                ampm={true}
+                value={end}
+              />
+            </LocalizationProvider>
+          </Grid>
+          <Grid item>
+            <FormControl sx={{ width: 204 }} size="small">
+              <InputLabel
+                className={classes.inputRoot}
+                id="status-multiple-checkbox-label"
+              >
+                Tag
+              </InputLabel>
+              <Select
+                labelId="status-multiple-checkbox-label"
+                id="status-multiple-checkbox"
+                multiple
+                value={statusList}
+                onChange={onStatusChange}
+                input={
+                  <OutlinedInput label="Tag" className={classes.inputRoot} />
+                }
+                renderValue={selected => selected.join(', ')}
+                inputProps={{ size: 'small' }}
+              >
+                {statuses.map(data => (
+                  <MenuItem key={data.status} value={data.status}>
+                    <Checkbox
+                      size={'small'}
+                      checked={statusList.indexOf(data.status) > -1}
+                    />
+                    <i
+                      className={classes.statusIcon}
+                      style={{ backgroundColor: data.color }}
+                    />
+                    <ListItemText
+                      primary={data.status}
+                      classes={{ primary: classes.inputRoot }}
+                    />
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item>
+            <FormControl sx={{ minWidth: 120 }} size="small">
+              <InputLabel
+                id="orderby-select-label-id"
+                className={classes.inputRoot}
+              >
+                Order by
+              </InputLabel>
+              <Select
+                labelId="orderby-select-label-id"
+                id="orderby-select-label"
+                value={orderBy}
+                label="Order by"
+                onChange={orderByChange}
+                input={
+                  <OutlinedInput
+                    label="Order by"
+                    className={classes.inputRoot}
                   />
-                  <ListItemText primary={data.status} />
+                }
+                inputProps={{ size: 'small' }}
+              >
+                <MenuItem value={'start_date'} className={classes.inputRoot}>
+                  Start Date
                 </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
+                <MenuItem value={'end_date'} className={classes.inputRoot}>
+                  End Date
+                </MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
         </Grid>
-        <Grid item>
-          <FormControl sx={{ mx: 1, minWidth: 120 }} size="small">
-            <InputLabel id="orderby-select-label-id">Order by</InputLabel>
-            <Select
-              labelId="orderby-select-label-id"
-              id="orderby-select-label"
-              value={orderBy}
-              label="Order by"
-              onChange={orderByChange}
-            >
-              <MenuItem value={'start_date'}>Start Date</MenuItem>
-              <MenuItem value={'end_date'}>End Date</MenuItem>
-            </Select>
-          </FormControl>
-        </Grid>
-      </Grid>
+      </FormControl>
     </Paper>
   );
 };
