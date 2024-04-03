f2937e42fa2b3b73b4d10f2940269921adfc4c7a
Ishwarya Surendrababu <sishwarya@digital.ai>
2024-04-03 16:49:29 +0530

    TK-89154 Error handling

--- plugins/dai-release/src/components/HomePageComponent/HomePageComponent.test.tsx
+++ plugins/dai-release/src/components/HomePageComponent/HomePageComponent.test.tsx
@@ -1,12 +1,13 @@
 import {
   renderInTestApp,
-  setupRequestMockHandlers,
+  setupRequestMockHandlers, TestApiProvider,
 } from '@backstage/test-utils';
 import { HomePageComponent } from './HomePageComponent';
 import React from 'react';
 import { rest } from 'msw';
-import { screen } from '@testing-library/react';
 import { setupServer } from 'msw/node';
+import {DiscoveryApi, discoveryApiRef} from "@backstage/core-plugin-api";
+import {DaiReleaseApiClient, daiReleaseApiRef} from "../../api";
 
 describe('HomePageComponent', () => {
   const server = setupServer();
@@ -15,12 +16,40 @@ describe('HomePageComponent', () => {
   // setup mock response
   beforeEach(() => {
     server.use(
-      rest.get('/*', (_, res, ctx) => res(ctx.status(200), ctx.json({}))),
+        rest.get(
+            'http://example.com/api/dai-release/releases',
+            (_, res, ctx) =>
+                res(
+                    ctx.status(200),
+                    ctx.set('Content-Type', 'application/json'),
+                    ctx.json({}),
+                ),
+        ),
     );
   });
 
   it('should render the home page', async () => {
-    await renderInTestApp(<HomePageComponent />);
-    expect(screen.getByText('Digital.ai Release')).toBeInTheDocument();
+    const rendered = await renderContent();
+    expect(rendered.getByText('Digital.ai Release')).toBeInTheDocument();
   });
 });
+
+const discoveryApi: DiscoveryApi = {
+  getBaseUrl: async () => 'http://example.com/api/dai-release',
+};
+
+async function renderContent() {
+  return await renderInTestApp(
+      <TestApiProvider
+          apis={[
+            [discoveryApiRef, discoveryApi],
+            [
+              daiReleaseApiRef,
+              new DaiReleaseApiClient({ discoveryApi}),
+            ],
+          ]}
+      >
+        <HomePageComponent />
+      </TestApiProvider>,
+  );
+}
