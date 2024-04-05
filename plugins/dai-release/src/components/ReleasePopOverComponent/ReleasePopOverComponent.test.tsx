27b10271e59376d5f28655623addf275ca39aba5
Ishwarya Surendrababu <sishwarya@digital.ai>
2024-04-05 10:07:46 +0530

    TK-89154 updated the test

--- plugins/dai-release/src/components/ReleasePopOverComponent/ReleasePopOverComponent.test.tsx
+++ plugins/dai-release/src/components/ReleasePopOverComponent/ReleasePopOverComponent.test.tsx
@@ -1,8 +1,13 @@
-import { fireEvent, render } from '@testing-library/react';
+import {fireEvent, render} from '@testing-library/react';
 import React from 'react';
 import { ReleasePopOverComponent } from './ReleasePopOverComponent';
 
 describe('ReleasePopOverComponent', () => {
+  it('should renders button', async () => {
+    const { getByLabelText } = render(<ReleasePopOverComponent />);
+    const button = getByLabelText('more');
+    expect(button).toBeInTheDocument();
+  });
   it('should render pop over with No data available', async () => {
     const { getByLabelText, getByText } = render(<ReleasePopOverComponent />);
     const button = getByLabelText('more');
