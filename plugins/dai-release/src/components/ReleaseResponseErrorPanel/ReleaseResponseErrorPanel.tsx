cf858e94dfbcf106917320dad2c5da30ac7dd7bc
sishwarya <63347232+sishwarya@users.noreply.github.com>
2024-05-21 14:37:07 +0530

    pre-alpha [S-110013] - ResponseErrorPanel with Header (#19)
    * S-110013 ResponseError with Header
    
    * fixing lint issue

--- plugins/dai-release/src/components/ReleaseResponseErrorPanel/ReleaseResponseErrorPanel.tsx
+++ plugins/dai-release/src/components/ReleaseResponseErrorPanel/ReleaseResponseErrorPanel.tsx
@@ -1,10 +1,36 @@
+import {Content, Header, Page, WarningPanel} from '@backstage/core-components';
+import { Paper, makeStyles} from "@material-ui/core";
 import React from 'react';
-import { WarningPanel } from '@backstage/core-components';
+import releaseLogoWhite from "../../assets/releaseLogoWhite.png";
 
 type ReleaseErrorPanelProps = {
   error: Error;
 };
+const useStyles = makeStyles(() => ({
+  logoStyle: {
+    width: '300px',
+  },
+}));
 export function ReleaseResponseErrorPanel(props: ReleaseErrorPanelProps) {
   const { error } = props;
-  return <WarningPanel title={error.message} />;
+  const classes = useStyles();
+  return (
+    <Page themeId="home">
+    <Header
+        title={
+          <img
+              src={releaseLogoWhite}
+              alt="Release logo"
+              className={classes.logoStyle}
+          />
+        }
+        pageTitleOverride="Digital.ai Release"
+    />
+    <Content>
+      <Paper elevation={1}>
+        <WarningPanel title={error.message} />
+      </Paper>
+    </Content>
+  </Page>
+);
 }
