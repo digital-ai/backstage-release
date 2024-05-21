ad8a98c792bb9a5253d28c76640e4d825eee697a
Ishwarya Surendrababu <sishwarya@digital.ai>
2024-05-21 13:46:48 +0530

    S-110013 ResponseError with Header

--- plugins/dai-release/src/components/ReleaseResponseErrorPanel/ReleaseResponseErrorPanel.tsx
+++ plugins/dai-release/src/components/ReleaseResponseErrorPanel/ReleaseResponseErrorPanel.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
-import { WarningPanel } from '@backstage/core-components';
+import {Content, Header, Page, WarningPanel} from '@backstage/core-components';
+import releaseLogoWhite from "../../assets/releaseLogoWhite.png";
+import { makeStyles, Paper} from "@material-ui/core";
 
 type ReleaseErrorPanelProps = {
   error: Error;
 };
+const useStyles = makeStyles(() => ({
+  logoStyle: {
+    width: '300px',
+  },
+}));
 export function ReleaseResponseErrorPanel(props: ReleaseErrorPanelProps) {
   const { error } = props;
-  return <WarningPanel title={error.message} />;
+  const classes = useStyles();
+  return <Page themeId="home">
+    <Header
+        title={
+          <img
+              src={releaseLogoWhite}
+              alt="Release logo"
+              className={classes.logoStyle}
+          />
+        }
+        pageTitleOverride="Digital.ai Release"
+    />
+    <Content>
+    <Paper elevation={1}>
+      <WarningPanel title={error.message} />
+    </Paper>
+    </Content>
+  </Page>
+
 }
