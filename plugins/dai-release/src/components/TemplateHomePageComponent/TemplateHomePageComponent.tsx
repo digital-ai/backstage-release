8cac3680febc8a6197b407e81a2092b8172bcd91
Ishwarya Surendrababu <ishwarya.surendrababu@digital.ai>
2024-09-17 16:21:52 +0530

    S-111249 - Fixing Data merging due to frequent dropdown change

--- plugins/dai-release/src/components/TemplateHomePageComponent/TemplateHomePageComponent.tsx
+++ plugins/dai-release/src/components/TemplateHomePageComponent/TemplateHomePageComponent.tsx
@@ -1,6 +1,6 @@
 import { Content, Header} from '@backstage/core-components';
 import {Grid, makeStyles,} from '@material-ui/core';
-import React, {useEffect} from 'react';
+import React from 'react';
 import { ReleaseResponseErrorPanel } from '../ReleaseResponseErrorPanel';
 import { ScrollableTable } from "../DenseScrollableTable/ScrollableTable";
 import { SearchHeaderComponent } from '../SearchHeaderComponent';
