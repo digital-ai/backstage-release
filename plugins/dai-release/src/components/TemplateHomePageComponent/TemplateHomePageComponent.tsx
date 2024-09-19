8eeb6042525acefff23601a037eebe5e1581f684
Ishwarya Surendrababu <ishwarya.surendrababu@digital.ai>
2024-09-19 13:01:48 +0530

    S-111249 - Review comments fixed ,to move plusIcon to commonplace

--- plugins/dai-release/src/components/TemplateHomePageComponent/TemplateHomePageComponent.tsx
+++ plugins/dai-release/src/components/TemplateHomePageComponent/TemplateHomePageComponent.tsx
@@ -10,26 +10,10 @@ import { ReleaseResponseErrorPanel } from '../ReleaseResponseErrorPanel';
 import { SearchHeaderComponent } from '../SearchHeaderComponent';
 import Typography from '@mui/material/Typography';
 import capitalize from 'lodash/capitalize';
-import { createSvgIcon } from '@mui/material/utils';
 import releaseLogoWhite from '../../assets/releaseLogoWhite.png';
 import { useTemplates } from '../../hooks';
+import {PlusIcon} from "../../icon/icon";
 
-const PlusIcon = createSvgIcon(
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    strokeWidth={1.5}
-    stroke="currentColor"
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M12 4.5v15m7.5-7.5h-15"
-    />
-  </svg>,
-  'Plus',
-);
 const useStyles = makeStyles(() => ({
   logoStyle: {
     width: '300px',
