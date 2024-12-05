dcb1e39651963c5b53ea36c69a207ad967364365
Nithesh <nithesh.vaddey@digital.ai>
2024-12-05 12:05:00 +0530

    Removed changes

--- plugins/dai-release/src/api/DaiReleaseApiClient.ts
+++ plugins/dai-release/src/api/DaiReleaseApiClient.ts
@@ -18,11 +18,28 @@ import { DaiReleaseApi } from './DaiReleaseApi';
 import { TemplateList } from '@digital-ai/plugin-dai-release-common';
 import { convertUnixTimestamp } from '../utils/dateTimeUtils';
 import dayjs from 'dayjs';
+import { workflowCatalogsList } from '../mocks/workflowMocks';
 
 export class DaiReleaseApiClient implements DaiReleaseApi {
   private readonly discoveryApi: DiscoveryApi;
-  private readonly identityApi: IdentityApi;
 
+
+
+
+
+
+
+          Expand Down
+
+
+
+
+
+          Expand Up
+
+    @@ -156,6 +155,48 @@ export class DaiReleaseApiClient implements DaiReleaseApi {
+
+  private readonly identityApi: IdentityApi;
   public constructor(options: {
     discoveryApi: DiscoveryApi;
     identityApi: IdentityApi;
@@ -30,16 +47,13 @@ export class DaiReleaseApiClient implements DaiReleaseApi {
     this.discoveryApi = options.discoveryApi;
     this.identityApi = options.identityApi;
   }
-
   private async getToken() {
     const { token } = await this.identityApi.getCredentials();
     return token;
   }
-
   private isStatusChecked(statusTags: string[], tag: string) {
     return encodeURIComponent(statusTags.indexOf(tag) > -1);
   }
-
   async getReleases(
     page: number,
     rowsPerPage: number,
@@ -71,16 +85,13 @@ export class DaiReleaseApiClient implements DaiReleaseApi {
     queryString.append('fromDate', convertUnixTimestamp(fromDate).toString());
     queryString.append('toDate', convertUnixTimestamp(toDate).toString());
     queryString.append('instanceName', instanceName.toString());
-
     const urlSegment = `releases?${queryString}`;
     const items = await this.get<ReleaseList>(urlSegment);
     return { items };
   }
-
   async getInstanceList(): Promise<ReleaseInstanceConfig[]> {
     return await this.get<ReleaseInstanceConfig[]>('instances');
   }
-
   async getTemplates(
     page: number,
     rowsPerPage: number,
@@ -90,7 +101,6 @@ export class DaiReleaseApiClient implements DaiReleaseApi {
     options?: { signal?: AbortSignal },
   ): Promise<{ items: TemplateList }> {
     const queryString = new URLSearchParams();
-
     queryString.append('pageNumber', page.toString());
     queryString.append('resultsPerPage', rowsPerPage.toString());
     queryString.append('title', searchTile.toString());
@@ -102,20 +112,17 @@ export class DaiReleaseApiClient implements DaiReleaseApi {
     const items = await this.get<TemplateList>(urlSegment, options);
     return { items };
   }
-
   async getTemplateMetaInfo(
     instanceName: string,
     folderId: string,
     options?: { signal?: AbortSignal },
   ): Promise<TemplateGitMetaInfo> {
     const queryString = new URLSearchParams();
-
     queryString.append('folderId', folderId.toString());
     queryString.append('instanceName', instanceName.toString());
     const urlSegment = `template/meta?${queryString}`;
     return await this.get<TemplateGitMetaInfo>(urlSegment, options);
   }
-
   private async get<T>(
     path: string,
     options?: { signal?: AbortSignal },
@@ -123,7 +130,6 @@ export class DaiReleaseApiClient implements DaiReleaseApi {
     const baseUrl = `${await this.discoveryApi.getBaseUrl('dai-release')}/`;
     const url = new URL(path, baseUrl);
     const idToken = await this.getToken();
-
     const response = await fetch(url.toString(), {
       method: 'GET',
       headers: {
@@ -133,7 +139,6 @@ export class DaiReleaseApiClient implements DaiReleaseApi {
       },
       signal: options?.signal,
     });
-
     if (!response.ok) {
       const data = await parseErrorResponseBody(response);
       if (response.status === 401) {
@@ -151,74 +156,43 @@ export class DaiReleaseApiClient implements DaiReleaseApi {
         `Unexpected error: failed to fetch data, status ${response.status}: ${response.statusText}`,
       );
     }
-
     return (await response.json()) as Promise<T>;
-  }
-
-  private async post<T>(
-    path: string,
-    body: string,
-    options?: { signal?: AbortSignal },
-  ): Promise<T> {
-    const baseUrl = `${await this.discoveryApi.getBaseUrl('dai-release')}/`;
-    const url = new URL(path, baseUrl);
-    const idToken = await this.getToken();
-    const bodyJson = body && body.length > 0 ? JSON.stringify(body) : JSON.stringify({});
-    const response = await fetch(url.toString(), {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-        Authorization: `Bearer ${idToken}`,
-      },
-      body: bodyJson,
-      signal: options?.signal,
-    });
-
-    if (!response.ok) {
-      const data = await parseErrorResponseBody(response);
-      if (response.status === 401) {
-        throw new AuthenticationError(data.error.message);
-      } else if (response.status === 403) {
-        throw new NotAllowedError(data.error.message);
-      } else if (response.status === 404) {
-        throw new NotFoundError(data.error.message);
-      } else if (response.status === 500) {
-        throw new ServiceUnavailableError(`Release Service Unavailable`);
-      } else if (response.status === 400) {
-        throw new InputError(data.error.message);
-      }
-      throw new Error(
-        `Unexpected error: failed to fetch data, status ${response.status}: ${response.statusText}`,
-      );
-    }
-
-    const jsonResponse = await response.json();
-    return jsonResponse as Promise<T>;
   }
 
   async getReleaseCategories(instanceName: string): Promise<ReleaseCategories> {
     const queryString = new URLSearchParams();
     queryString.append('instanceName', instanceName.toString());
+
+
+
+
+
+
+
+        Expand All
+
+    @@ -165,20 +206,19 @@ export class DaiReleaseApiClient implements DaiReleaseApi {
+
     const urlSegment = `categories?${queryString}`;
     return await this.get<ReleaseCategories>(urlSegment);
   }
 
   async getWorkflowCatalog(
     page: number,
-    rowsPerPage: number,
     searchInput: string,
     categories: string[],
     author: string,
     instanceName: string,
-    options?: { signal?: AbortSignal }
   ): Promise<WorkflowsList> {
     const queryString = new URLSearchParams();
-    queryString.append("instanceName", instanceName.toString());
-    queryString.append("pageNumber", page.toString());
-    queryString.append("resultsPerPage", rowsPerPage.toString());
-    queryString.append("searchInput", rowsPerPage.toString());
-    const urlSegment = `workflows?${queryString}`;
-    return await this.post(urlSegment, "", options);
+    queryString.append('instanceName', instanceName.toString());
+    const urlSegment = `workflows?pageNumber=${page}&searchInput=${searchInput}&categories=${categories}&author=${author}`;
+    // will be removed in next PR by implementing the actual API call
+    global.console.log('urlSegment', urlSegment);
+    const response = new Response(JSON.stringify(workflowCatalogsList), {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' },
+    });
+    return (await response.json()) as Promise<WorkflowsList>;
   }
 }
